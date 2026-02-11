3cb4f4b5b30fdb82cb4da081d5b7d7b438fad96d
Ademir Patricio <falecom@ademirpatricio.com.br>
2026-02-11 15:31:15 -0300

    alterando os títulos da página

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
+// Pegando a URL atual
 let urlAtual = window.location.href;
-let meuLink = document.getElementById("meuLink");
-meuLink.innerText = urlAtual;
 
-//let urlTratada = urlAtual.toString();
-//console.log(urlTratada);
+//Verificando qual página o usuáio está:
+let tituloPergunta;
+
+if (urlAtual.includes("recife")) {
+    tituloPergunta = "carnavalrecife.com.br";
+} else {
+    tituloPergunta = "carnavaldeolinda.com.br";
+};
+
+// Alterando para maiúscula
+const resultado = tituloPergunta.charAt(0).toUpperCase() + tituloPergunta.slice(1);
+
+// Mudando o h1 da página
+let meuLink = document.getElementById("meuLink");
+meuLink.innerText = tituloPergunta;
+
+// Mudando o título da página
+let titulo = "🎉 " + resultado + " | Malabares MKT & TEC";
+document.title = titulo;
