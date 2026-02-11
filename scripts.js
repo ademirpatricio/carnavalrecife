f118371dca21870914ba74eebb23167c579b914e
Ademir Patricio <falecom@ademirpatricio.com.br>
2026-02-11 09:35:31 -0300

    atualizando arquivos para novo projeto

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let urlAtual = window.location.href;
+let meuLink = document.getElementById("meuLink");
+meuLink.innerText = urlAtual;
+
+//let urlTratada = urlAtual.toString();
+//console.log(urlTratada);
